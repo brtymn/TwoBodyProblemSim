cfb912c3325869959ef30df5902c3869d7d12e27
brtymn <yaman.bartu@metu.edu.tr>
2020-08-12 02:58:21 +0300

    Added second draft.

--- sketch.js
+++ sketch.js
@@ -1,89 +1,85 @@
 let time_step = 0.5;
-let gravitational_constant = 1000000;
+let gravitational_constant = 1000;
 let body_one;
 let body_two;
 
 
 function setup() {
-  createCanvas(800, 800);
-  
-  body_one = new Body(100, 200, 100, 100);
-  body_two = new Body(300, 400, 100, 100);
-  
-  
+  createCanvas(1000, 1000);
+
+  body_one = new Body(50, 700, 5, -5, 10, 300, 0); // Black
+
+  body_two = new Body(800, 100, -5, 5, 10, 300, 255); // White
+
+
 }
 
 function draw() {
   background(50, 89, 100);
-  
-  move_the_system(body_one, body_two);
-  
+
+  move(body_one, body_two);
+
   body_one.display_bodies();
+
   body_two.display_bodies();
-  
-  
-  
+
+
 }
 
 
-function calculate_distance(body1, body2, n){
-  
+function move(body1, body2){
+
   let body1_pos = body1.position_vector;
   let body2_pos = body2.position_vector;
-  
+
   let body1_pos_to_body2_pos = p5.Vector.sub(body1_pos, body2_pos);
   let body2_pos_to_body1_pos = p5.Vector.sub(body2_pos, body1_pos);
-    
-  if (n == 12){
-    return body1_pos_to_body2_pos;
-  }
-  else if (n == 21){
-    return body2_pos_to_body1_pos;
-  }
-  
-}
+
+  let dist = body1_pos_to_body2_pos.mag();
+  let norm_12 = body1_pos_to_body2_pos.normalize();
+  let norm_21 = body2_pos_to_body1_pos.normalize();
 
 
-function calculate_forces(body1, body2, n){
-  
-  let dist = calculate_distance(body1, body2, 12).mag();
-  let norm_12 = calculate_distance(body1, body2, 12).normalize();
-  let norm_21 = calculate_distance(body1, body2, 21).normalize();
-  
-  acceleration_1 = ((norm_21.mult(gravitational_constant * body1.mass * body2.mass)).div(pow(dist, 3) * body1.mass));
-  
-  acceleration_2 = ((norm_12.mult(gravitational_constant * body1.mass * body2.mass)).div(pow(dist, 3) * body2.mass));
-  
-  if (n == 1){
-    return acceleration_1;
-  }
-  else if (n == 2){
-    return acceleration_2;
-  }
-
-  
-}
-
-
-function move_the_system(body1, body2){
-  
-  let pos_1 = body1.position_vector;
   let velo_1 = body1.velocity_vector;
-  let acc_1 = calculate_forces(body1, body2, 1);
-  let pos_2 = body2.position_vector;
-  let velo_2 = body1.velocity_vector;
-  let acc_2 = calculate_forces(body1, body2, 2);
-  
-  pos_1.add(velo_1.mult(time_step).add(acc_1.mult(pow(time_step, 2)).div(2)));
-  
-  velo_1.add(velo_1.add(acc_1.mult(time_step).div(2)));
-  
-  
-  
-  pos_2.add(velo_2.mult(time_step).add(acc_2.mult(pow(time_step, 2)).div(2)));
-  
-  velo_2.add(velo_2.add(acc_2.mult(time_step).div(2)));
-  
+  let velo_2 = body2.velocity_vector;
+
+  let norm_12_array = norm_12.array();
+  let norm_21_array = norm_21.array();
+
+
+  let pos_1_array = body1_pos.array();
+  let pos_2_array = body2_pos.array();
+
+  acceleration_1 = ((norm_21.mult(gravitational_constant * body1.mass * body2.mass)).div(pow(dist, 3) * body1.mass));
+
+  acceleration_2 = ((norm_12.mult(gravitational_constant * body1.mass * body2.mass)).div(pow(dist, 3) * body2.mass));
+
+
+  body1_pos.add(velo_1.mult(time_step).add(acceleration_1.mult(pow(time_step, 2)).div(2)));
+
+  velo_1.add(velo_1.add(acceleration_1.mult(time_step).div(2)));
+
+
+
+  body2_pos.add(velo_2.mult(time_step).add(acceleration_2.mult(pow(time_step, 2)).div(2)));
+
+  velo_2.add(velo_2.add(acceleration_2.mult(time_step).div(2)));
+
+
+  if (pos_1_array[0] > width - 10 || pos_1_array[0] < 10) {
+    norm_12_array[0] *=(-1);
+  }
+  if (pos_1_array[1] > height - 10 || pos_1_array[1] < 10)  {
+    norm_12_array[1] *= (-1);
+  }
+
+  if (pos_2_array[0] > width - 10 || pos_2_array[0] < 10) {
+    norm_21_array[0] *=(-1);
+  }
+  if (pos_2_array[1] > height - 10 || pos_2_array[1] < 10)  {
+    norm_21_array[1] *= (-1);
+  }
+
 
 
 }
